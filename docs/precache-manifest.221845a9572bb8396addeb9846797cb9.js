5400a51eb346b37f8cc1d90614faac78f6c7b64b
rj-maitra <zzyxer@gmail.com>
2019-07-05 12:02:37 -0400

    added docs

---
+++ docs/precache-manifest.221845a9572bb8396addeb9846797cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6fbb60c34dc3e58c8fcfda2987754e",
+    "url": "/react-ticking-clock/index.html"
+  },
+  {
+    "revision": "de10752e08ad506ed526",
+    "url": "/react-ticking-clock/static/css/main.179bb7ef.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-ticking-clock/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "de10752e08ad506ed526",
+    "url": "/react-ticking-clock/static/js/main.016deb8b.chunk.js"
+  },
+  {
+    "revision": "cd6b02da3de039daf264",
+    "url": "/react-ticking-clock/static/js/runtime~main.cd10d4cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ticking-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
